8536947a74369bdd43d27030803999e275c7daf5
navin136 <nkwhitehat@gmail.com>
2022-03-08 18:56:51 +0530

    Refactor and add some features

--- compilerom.sh
+++ compilerom.sh
@@ -17,86 +17,46 @@ git clone https://github.com/navin136/scripts.git scripts && cd scripts
 bash setup/android_build_env.sh
 cd
 rm -rf scripts
-else
 git config --global user.name "navin136"
 git config --global user.email "nkwhitehat@gmail.com"
 git config --global credential.helper store
-echo "Which Rom Would You Want To Build?"
-echo "1.Evolution-X"
-echo "2.Spice OS"
-echo "Enter Your Option: "
-read option
-if [ $option -eq 1 ]
-then
-export BUILD=evo
-fi
-if [ $option -eq 2 ]
-then
-export BUILD=spice
-fi
-if [ $BUILD = evo ]
-then
-cd
-if [ -d evo ]
-then
-cd evo
-export ROM=$(pwd)
-rm -rf $ROM/device/asus/X00TD && git clone https://github.com/navin136/android_device_asus_X00TD -b snow $ROM/device/asus/X00TD && rm -rf $ROM/vendor/asus && git clone https://github.com/navin136/android_vendor_asus_X00TD -b snow $ROM/vendor/asus --depth=1 && rm -rf $ROM/kernel/asus/sdm660 && git clone https://github.com/navin136/android_kernel_asus_X00TD -b snow $ROM/kernel/asus/sdm660 --depth=1
-rm -rf $ROM/hardware/qcom-caf/msm8998/audio && git clone https://github.com/navin136/android_hardware_qcom-caf_audio_msm8998 $ROM/hardware/qcom-caf/msm8998/audio && rm -rf $ROM/hardware/qcom-caf/msm8998/display && git clone https://github.com/navin136/android_hardware_qcom-caf_display_msm8998 $ROM/hardware/qcom-caf/msm8998/display
-sudo mkdir /mnt/ccache
-sudo mount ../.cache /mnt/ccache
-export CCACHE_DIR=/mnt/ccache
-. b*/e*
-lunch evolution_X00TD-userdebug
-make evolution-prod -j$(nproc --all) | tee log.txt
 else
-mkdir evo
-cd evo
+read -p "Give me Repo link of Rom :" repoLink
+read -p "Which branch of Repo You want to Sync:" repoBranch
+romDir=$(echo $repoLink|cut -d "/" -f 4)
+if [ -d $romDir ]
+then
+cd $romDir
 export ROM=$(pwd)
-repo init -u https://github.com/Evolution-X/manifest -b snow --depth=1
-repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
-rm -rf $ROM/device/asus/X00TD && git clone https://github.com/navin136/android_device_asus_X00TD -b snow $ROM/device/asus/X00TD && rm -rf $ROM/vendor/asus && git clone https://github.com/navin136/android_vendor_asus_X00TD -b snow $ROM/vendor/asus --depth=1 && rm -rf $ROM/kernel/asus/sdm660 && git clone https://github.com/navin136/android_kernel_asus_X00TD -b snow $ROM/kernel/asus/sdm660 --depth=1
-rm -rf $ROM/hardware/qcom-caf/msm8998/audio && git clone https://github.com/navin136/android_hardware_qcom-caf_audio_msm8998 $ROM/hardware/qcom-caf/msm8998/audio && rm -rf $ROM/hardware/qcom-caf/msm8998/display && git clone https://github.com/navin136/android_hardware_qcom-caf_display_msm8998 $ROM/hardware/qcom-caf/msm8998/display
-git clone https://github.com/Navin136/vendor_evolution_build_target_product_security $ROM/vendor/evolution/build/target/product/security
-git clone https://gitlab.com/varunhardgamer/trb_clang -b 14 prebuilts/clang/host/linux-x86/trb_clang
+rm -rf $ROM/device/asus/X00T && git clone https://github.com/X00T-dev/device_asus_X00T -b 12 $ROM/device/asus/X00T && rm -rf $ROM/vendor/asus && git clone https://github.com/X00T-dev/vendor_asus -b 12 $ROM/vendor/asus --depth=1 && rm -rf $ROM/kernel/asus/sdm660 && git clone https://github.com/arrowos-devices/android_kernel_asus_X00T $ROM/kernel/asus/sdm660 --depth=1
+rm -rf $ROM/hardware/qcom-caf/sdm660/audio && git clone https://github.com/X00T-dev/hardware_qcom-caf_sdm660_audio $ROM/hardware/qcom-caf/sdm660/audio && rm -rf $ROM/hardware/qcom-caf/sdm660/display && git clone https://github.com/navin136/hardware_qcom-caf_sdm660_display $ROM/hardware/qcom-caf/sdm660/display
 sudo mkdir /mnt/ccache
 sudo mount ../.cache /mnt/ccache
 export CCACHE_DIR=/mnt/ccache
+romVendorName=$(ls device/asus/X00T/*_X00T.mk | cut -d "/" -f 4 | cut -d "_" -f 1)
 . b*/e*
-lunch evolution_X00TD-userdebug
-make evolution-prod -j$(nproc --all) | tee log.txt
-fi
-fi
-if [ $BUILD = spice ]
-then
-cd
-if [ -d spice ]
-then
-cd spice
-export ROM=$(pwd)
-rm -rf $ROM/device/asus/X00TD && git clone https://github.com/navin136/android_device_asus_X00TD -b spiceos $ROM/device/asus/X00TD && rm -rf $ROM/vendor/asus && git clone https://github.com/navin136/android_vendor_asus_X00TD -b snow $ROM/vendor/asus --depth=1 && rm -rf $ROM/kernel/asus/sdm660 && git clone https://github.com/navin136/android_kernel_asus_X00TD -b snow $ROM/kernel/asus/sdm660 --depth=1
-rm -rf $ROM/hardware/qcom-caf/msm8998/audio && git clone https://github.com/navin136/android_hardware_qcom-caf_audio_msm8998 $ROM/hardware/qcom-caf/msm8998/audio && rm -rf $ROM/hardware/qcom-caf/msm8998/display && git clone https://github.com/navin136/android_hardware_qcom-caf_display_msm8998 $ROM/hardware/qcom-caf/msm8998/display
-sudo mkdir /mnt/ccache
-sudo mount ../.cache /mnt/ccache
-export CCACHE_DIR=/mnt/ccache
-. b*/e*
-lunch lineage_X00TD-user
+lunch $(romVendorName)_X00T-userdebug
 make bacon -j$(nproc --all) | tee log.txt
 else
-mkdir spice
-cd spice
+mkdir $romDir
+cd $romDir
 export ROM=$(pwd)
-repo init -u https://github.com/SpiceOS/android.git -b 11 --depth=1
+repo init -u $repoLink -b $repoBranch --depth=1
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
-rm -rf $ROM/device/asus/X00TD && git clone https://github.com/navin136/android_device_asus_X00TD -b spiceos $ROM/device/asus/X00TD && rm -rf $ROM/vendor/asus && git clone https://github.com/navin136/android_vendor_asus_X00TD -b snow $ROM/vendor/asus --depth=1 && rm -rf $ROM/kernel/asus/sdm660 && git clone https://github.com/navin136/android_kernel_asus_X00TD -b snow $ROM/kernel/asus/sdm660 --depth=1
-rm -rf $ROM/hardware/qcom-caf/msm8998/audio && git clone https://github.com/navin136/android_hardware_qcom-caf_audio_msm8998 $ROM/hardware/qcom-caf/msm8998/audio && rm -rf $ROM/hardware/qcom-caf/msm8998/display && git clone https://github.com/navin136/android_hardware_qcom-caf_display_msm8998 $ROM/hardware/qcom-caf/msm8998/display
-git clone https://gitlab.com/varunhardgamer/trb_clang -b 14 prebuilts/clang/host/linux-x86/trb_clang
+rm -rf $ROM/device/asus/X00T && git clone https://github.com/X00T-dev/device_asus_X00T -b 12 $ROM/device/asus/X00T && rm -rf $ROM/vendor/asus && git clone https://github.com/X00T-dev/vendor_asus -b 12 $ROM/vendor/asus --depth=1 && rm -rf $ROM/kernel/asus/sdm660 && git clone https://github.com/arrowos-devices/android_kernel_asus_X00T $ROM/kernel/asus/sdm660 --depth=1
+rm -rf $ROM/hardware/qcom-caf/sdm660/audio && git clone https://github.com/X00T-dev/hardware_qcom-caf_sdm660_audio $ROM/hardware/qcom-caf/sdm660/audio && rm -rf $ROM/hardware/qcom-caf/sdm660/display && git clone https://github.com/navin136/hardware_qcom-caf_sdm660_display $ROM/hardware/qcom-caf/sdm660/display
 sudo mkdir /mnt/ccache
 sudo mount ../.cache /mnt/ccache
 export CCACHE_DIR=/mnt/ccache
+romVendorName=$(ls device/asus/X00T/*_X00T.mk | cut -d "/" -f 4 | cut -d "_" -f 1)
 . b*/e*
-lunch lineage_X00TD-user
+lunch $(romVendorName)_X00T-userdebug
 make bacon -j$(nproc --all) | tee log.txt
 fi
 fi
 fi
+curl --upload-file out/target/product/X00T/*X00T*.zip https://free.keep.sh/$romVendorName*X00T*.zip > build.txt
+function sendLink(){
+    curl -X POST https://api.telegram.org/bot1906657636:AAEl8FIiy0kDff0pFoLodnO-Vwg6JNG5FZg/sendMessage -d chat_id="-1001509079419" -d text=$1
+}
+sendLink $(cat build.txt)
